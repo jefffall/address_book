2514f1b3509ff72a530351529e88621bc5656307
Sam Pierson <sam@sampierson.com>
2011-08-07 12:09:27 -0700

    rails new address_book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressBook::Application.config.secret_token = 'd0d7e220d05b7c7f6ee96918f2448bae2c9abd0e716bea7ce589b9ea177a98a1f3dfade8b16d48754fe4295c56db188378dec58a102c7e5afe9bafb31534307d'
