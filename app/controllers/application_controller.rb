f59a099be33fc96017812a9ff49a6844c34346ad
Sam Pierson <sam@sampierson.com>
2011-08-07 12:25:37 -0700

    Wrap site in basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :wrap_site_in_basic_auth
+
+  def wrap_site_in_basic_auth
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == "jfall" && password == "jfall"
+    end
+  end
 end
